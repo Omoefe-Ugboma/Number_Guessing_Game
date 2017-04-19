1b86aa5c996678f894ede738f875b3ed8436de1f
Omoefe Joseph Ugboma <jkomoefey23e@yahoo.co.uk>
2017-04-18 17:35:29 -0700

    updated all file now

---
+++ index.php
@@ -0,0 +1,10 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title> E - Commerce Application </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+My app test
+</body>
+</html>
