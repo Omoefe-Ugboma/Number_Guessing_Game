b104a88cb50e8376e816885ba747fc485eba0e1f
Omoefe Ugboma <jkomoefey23e@yahoo.co.uk>
2017-04-19 14:36:29 +0100

    Adding start text file

--- index.php
+++ index.php
@@ -9,23 +9,105 @@
 <div>
 
     <div>
-        <div>Top Header 1</div>
-        <div>Top Header 2</div>
-        <div>Top Header 3</div>
+        <section>
+          <header>
+             <div id="logo">Company Logo</div>
+             <div>Seach section</div>
+             <div>MoneyBox Wallet</div>
+             <div>No Item in Your Bag</div>
+              <div>LogIn/SignUp</div>
+          </header>
+        </section>
+       <div>
+        <section>
+          <header>
+             <nav class="NavMenu">
+                <ul>
+                  <li>Mobile</li>
+                  <li>Electricity</li>
+                  <li>DTH</li>
+                  <li>Landline</li>
+                  <li>Financial services</li>
+                  <li>Gas</li>
+                </ul>
+            </nav>
+          </header>
+        </section>
+      </div>
+      <section>
+        <div>
+           <div class="verticalMenu">Our Product</div>
+           <div class="carousel">Center Slider</div>
+           <div class="RightAbs">
+            <aside>
+              <div class="marketPlace">Right Side ads</div>
+            </aside>
+           </div>
+        </div>
+      </section>
     </div>
-
+<section>
     <div>
-        <div>Top Header 1</div>
-        <div>Top Header 2</div>
-        <div>Top Header 3</div>
+        <div>Shop on MoneyBox Section</div>
+        <div>Top outfit of the year</div>
+        <div>Electronics Section</div>
+        <div>To Fashion Offers</div>
+        <div>Top Home & Kitchen Offers</div>
+        <div>Utilities</div>
+        <div>Travel</div>
+        <div>Offers on movie</div>
+        <div>Recharge</div>
+        <div>Pay with MoneyBox</div>
+        <div>Our Suggestion for you</div>
+        <div>International Products Limited Time Offers</div>
+        <div>Deals Near You</div>
+        <div>Deals Near You</div>
     </div>
+</section>
 
+<section>
     <div>
-        <div>Top Header 1</div>
-        <div>Top Header 2</div>
-        <div>Top Header 3</div>
+        <div>Go Easy on your Pocket – Online Recharge, Utility Bill Payment & Booking in the Blink of an Eye</div>
+        <div>Mobile, DTH and Data Card Recharge & Payment On-The-Go</div>
+        <div>Quick Utility Bill, Insurance, Loan & Education Fee Payment</div>
+        <div>Get Bus, Train & Flights Tickets and Make Hotel Booking with Great Ease</div>
+        <div>Live, Laugh, Enjoy with MoneyBox.com!</div>
     </div>
-
+</section>
+<section>
+   <div class="FooterNavMenu">
+     <nav>
+       <ul>
+         <li>About Us</li>
+         <li>Partner with us</li>
+         <li>Terms & Conditions</li>
+         <li>Blog</li>
+         <li>Media</li>
+         <li>Customer service</li>
+         <li>Grievance policy</li>
+         <li>Bug bounty</li>
+         <li>Recharge Partners</li>
+       </ul>
+       <div class="socialnetwork">
+       <ul>
+         <li>Twitter</li>
+         <li>Facebook</li>
+         <li>Pinterest</li>
+         <li>Instagram</li>
+         <li>Apple</li>
+         <li>Windows</li>
+         <li>Android</li>
+         <li>IOS</li>
+       </ul>
+     </div>
+   </nav>
+   <section>
+    <footer>
+     <div>Footer section for MoneyBox</div>
+    </footer>
+   </section>
+   <div>
+</section>
 </div>
 </body>
 </html>
