ef8d0fa8918a5baa31c7957854219cdedde7e31c
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-08-03 21:13:41 +0100

    update on the js 1

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ GAME FUNCTION
  -Notify the player of guesses remaining 
  -Notify the player of the correct answer if loose
  -Let player choose to play again
-*/ 
+*/
 
 // Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.getElementById('game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
